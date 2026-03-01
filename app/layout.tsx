d14359cfc27f47358c71fee06dd55dd089a43775
FurkiDeniz <106449424+FurkiDeniz@users.noreply.github.com>
2026-03-01 19:14:17 +0300

    feat: Initialize Next.js root layout with global styles, fonts, metadata, and essential providers.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,19 +19,18 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/icon-light.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/icon-dark.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/icon.png',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/icon-dark.png',
   },
 }
 
